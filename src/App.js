b61e1615e4a80a4414bc903dddb67e5b59f7ddd3
sirjan <sirjan.gnr@gmail.com>
2022-06-20 16:00:06 +0545

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to sirjan hahahah to reload.
+          Edit <code>src/App.js</code> and save to sirjaaasdsada sd asdas da sn hahahah to reload.
         </p>
         <a
           className="App-link"
